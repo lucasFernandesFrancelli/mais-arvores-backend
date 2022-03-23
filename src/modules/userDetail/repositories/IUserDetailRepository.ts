eaa794c3dd63e94ce118fe7957d7093a947cb763
lucasFernandesFrancelli <81181652+lucasFernandesFrancelli@users.noreply.github.com>
2022-03-22 21:58:01 -0300

    feat(userDetail): implements userDetail (#4)
    Co-authored-by: unknown <32620@fiecalunos.lan>

--- src/modules/userDetail/repositories/IUserDetailRepository.ts
+++ src/modules/userDetail/repositories/IUserDetailRepository.ts
@@ -3,4 +3,6 @@ import { IUserDetailDTO } from '../dtos/IUserDetailDTO';
 export interface IUserDetailRepository {
   save(userDetail: IUserDetailDTO): Promise<IUserDetailDTO>;
   list(): Promise<IUserDetailDTO[]>;
+  findById(id: string): Promise<IUserDetailDTO | undefined>;
+  update(id: string, userDetail: IUserDetailDTO): Promise<void>;
 }
