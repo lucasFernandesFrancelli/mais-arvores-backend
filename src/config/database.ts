8be5be745215f0b2362203bcb037bae7ae257a0b
lucasFernandesFrancelli <luffbsilva2002@gmail.com>
2022-04-18 08:43:09 -0300

    fix(Ormconfig): Fixing error on ormconfig

--- src/config/database.ts
+++ src/config/database.ts
@@ -8,12 +8,12 @@ export default (): {
   PASSWORD: string;
 } => {
   return {
-    DIALECT: process.env.DB_DIALECT ?? 'mysql',
-    HOST: process.env.DB_HOST ?? 'localhost',
-    PORT: Number(process.env.DB_PORT) ?? 3306,
-    NAME: process.env.DB_NAME ?? '',
+    DIALECT: process.env.DB_DIALECT || 'mysql',
+    HOST: process.env.DB_HOST || 'localhost',
+    PORT: Number(process.env.DB_PORT) || 3306,
+    NAME: process.env.DB_NAME || '',
     TEST_NAME: process.env.DB_TEST_NAME ?? '',
-    USER: process.env.DB_USER ?? '',
+    USER: process.env.DB_USER || '',
     PASSWORD: process.env.DB_PASSWORD ?? '',
   };
 };
